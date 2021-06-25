8064d6deae149be25432b05ebd5063d8f5d00a21
David Bento <david.bento@concil.com.br>
2021-06-25 09:37:15 -0300

    ajustes no layout e inclusão de novas sections

--- scripts/main.js
+++ scripts/main.js
@@ -27,3 +27,14 @@ window.addEventListener('scroll', function () {
     header.classList.remove('scroll')
   }
 })
+
+// TESTIMONIALS CAROUSEL
+
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
